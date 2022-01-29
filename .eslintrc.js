87b5079c04773c5ebd83a55709b5c292673b6e55
wallace-m-brandon <98620383+wallace-m-brandon@users.noreply.github.com>
2022-01-29 16:18:42 -0500

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
     ],
     extends: [
       'eslint:recommended',
+      'plugin:@next/next/recommended',
       'plugin:@typescript-eslint/recommended',
       'prettier',
     ],
